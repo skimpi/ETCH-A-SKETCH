0e8dabc09e630ab215c3bce0350756b4b5c84f2a
skimpi <56881920+skimpi@users.noreply.github.com>
2020-01-04 16:01:17 +0100

    almost final
    needs rework and a missing feature of black colour with 10% opacity function

--- README.md
+++ README.md
@@ -2,6 +2,20 @@
 odin project
 looooppp
 
+
+
+const grid = () => {
+    const container = document.querySelector(".grid-container");
+
+    for (i = 0; i < 56; i++) {
+       let item = document.createElement('div');
+       item.className = 'item';
+       item.id = `grid${i}`;
+       container.appendChild(item);
+       
+       }; // this creates a number of grids
+
+    };
 /*
 const item = container.querySelectorAll('.item'); //summons the class item as a nodelist (contains all 36 individual item)
 let nodeList = item;
@@ -50,3 +64,36 @@ const rebeccapurple = () => {
     
 rebeccapurple();
 
+
+
+// Sets important constants and variables
+const container = document.getElementById("container");
+let rows = document.getElementsByClassName("gridRow");
+let cells = document.getElementsByClassName("cell");
+console.log(container);
+ defaultGrid();
+// Creates a default grid sized 16x16
+function defaultGrid() {
+  makeRows(16);
+  makeColumns(16);
+};
+// Takes (rows, columns) input and makes a grid
+function makeRows(rowNum) {
+  // Creates rows
+  for (r = 0; r < rowNum; r++) {
+    let row = document.createElement("div");
+    container.appendChild(row).className = "gridRow";
+  };
+};
+
+//Creates columns sååååå jävla smart!!!
+function makeColumns(cellNum) {
+  for (i = 0; i < rows.length; i++) {
+    for (j = 0; j < cellNum; j++) {
+      let newCell = document.createElement("div");
+      rows[j].appendChild(newCell).className = "cell";
+    };
+  };
+};
+
+console.log("JavaScript is amazing!");
--- script.js
+++ script.js
@@ -1,43 +1,175 @@
 
-const grid = () => {
-    const container = document.querySelector(".grid-container");
-
-    for (i = 0; i < 56; i++) {
-       let item = document.createElement('div');
-       item.className = 'item'
-       item.id = `grid${i}`;
-       container.appendChild(item);
-       
-       }; // this creates a number of grids
-
-    };
-
-grid();
-
-
 var input = document.querySelectorAll('input');
+let display = document.getElementById('display');
+let size = document.querySelector('.size');
+const container = document.getElementById("container");
+
+
+
+function makeRows(rows, cols) {
+  container.style.setProperty('--grid-rows', rows);
+  container.style.setProperty('--grid-cols', cols);
+  for (c = 0; c < (rows * cols); c++) {
+    let cell = document.createElement("div");
+    cell.innerText = (c + 1); //numrering av individuell block
+    container.appendChild(cell).classList = 'grid-item';// lägger den nya element i elementet container
+  };
+};
+
+makeRows(5, 5);
+
+const canvaSize = () => {
+    size.addEventListener('click', function(){
+       //let bajs = document.querySelector('.grid-item');
+       
+       while (container.firstChild) {
+           container.removeChild(container.firstChild);
+       };
+        let size = prompt('size');
+        let canvas = size * size;
+        let cols = size;
+        let rows = size;
+        if (size != 0) {
+            document.getElementById('demo').innerHTML = 'size: ' + canvas; 
+            
+            container.style.setProperty('--grid-rows', rows);
+            container.style.setProperty('--grid-cols', cols);
+            
+            
+            for (c = 0; c < (rows * cols); c++) {
+              let cell = document.createElement("div");
+              cell.innerText = (c + 1); //numrering av individuell block
+              container.appendChild(cell).classList = 'grid-item';
+              
+            };
+            const cells = container.querySelectorAll('.grid-item');
+              console.log(cells)
+              color();
+              reset();
+              random();
+              black();
+            
+        };
+    });
+
+};
+canvaSize();
 
 for (let i = 0; i < input.length; i++){
     input[i].addEventListener('input', function(){
-        let red = document.getElementById('red').value,
+
+       let red = document.getElementById('red').value,
             green = document.getElementById('green').value,
             blue = document.getElementById('blue').value;
         
-        let display = document.getElementById('display');
+        
         display.style.background = "rgb("+ red +", "+ green +", "+ blue +")";
     });
-}
+}; // color value for display
 
-const container = document.querySelector('.grid-container'); //summons the class grid-container from index.html
-const item = container.querySelectorAll('.item');
+
 
 const color = () => {
-    container.querySelectorAll('.item').forEach(item => {
-        item.addEventListener('mouseover', function (q) {
-            q.target.classList = 'colord';
-        })
-    })
-};
+    
+    container.querySelectorAll('.grid-item').forEach(cells => {
+
+        for (let i = 0; i < input.length; i++) {
+            input[i].addEventListener('input', function(){
+
+                let red = document.getElementById('red').value,
+                    green = document.getElementById('green').value,
+                    blue = document.getElementById('blue').value;
+
+                    cells.addEventListener('mouseover', function (q) {
+                        q.target.style.background = "rgba("+ red +", "+ green +", "+ blue +")";
+                    });
+
+            });
+        };
+    });
+}; // color value for grids
 
 color();
 
+const reset = () => {
+    const resetButton = document.querySelector('.resetButton');
+    
+    container.querySelectorAll('.grid-item').forEach(cells => {
+        resetButton.addEventListener('click', function () {
+            cells.style.background = "rgba("+ 255 +", "+ 255 +", "+ 255 +", "+ 0.8 +")";
+        });
+    })
+};
+
+reset();
+
+const random = () => {
+    
+    container.querySelectorAll('.grid-item').forEach(cells => {
+
+        for (let i = 0; i < 3; i++) {
+            const randomColor = document.querySelector('.random');
+            randomColor.addEventListener('click', function(){
+
+                let red = Math.floor(Math.random() * 255),
+                    green = Math.floor(Math.random() * 255),
+                    blue = Math.floor(Math.random() * 255);
+
+                    cells.addEventListener('mouseover', function (q) {
+                        q.target.style.background = "rgba("+ red +", "+ green +", "+ blue +")";
+                    });
+
+            });
+        };
+    });
+}; // color value for grids
+console.log(input.length)
+random();
+
+const black = () => {
+    
+    container.querySelectorAll('.grid-item').forEach(cells => {
+
+            const black = document.querySelector('.black');
+            black.addEventListener('click', function(){
+
+                let red = 0,
+                green = 0,
+                blue = 0,
+                alpha = 0;
+
+                    cells.addEventListener('mouseover', function (q) {
+                        q.target.style.backgroundColor = "rgba(0, 0, 0, 1)";
+                    });
+
+            });
+    });
+}; // color value for grids
+black();
+
+function alphaBlack(q) {
+
+    
+        let currentColor = window.getComputedStyle(q.target ,null).getPropertyValue('background-color');
+        if(currentColor === "rgba(0, 0, 0, 0)"){
+            q.target.style.backgroundColor = "rgba(0, 0, 0, 1)";
+        }else{
+            return null;
+        }
+    
+
+}
+
+
+function myFunction() {
+    var elem = document.getElementById("test");
+    var txt;
+    cssObj = window.getComputedStyle(elem, null)
+
+    for (i = 0; i < cssObj.length; i++) {
+        cssObjProp = cssObj.item(i)
+        txt += cssObjProp + " = " + cssObj.getPropertyValue(cssObjProp) + "<br>";
+    }
+    document.getElementById("demo").innerHTML = txt;
+
+}
